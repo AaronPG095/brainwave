2e974aa23c82b66eb0780b72ba012f98ef864189
Aaron Paul Greyling <aaron.p.greyling@gmail.com>
2024-05-14 21:33:34 +0200

    Started Hero Section

---
+++ src/components/Hero.jsx
@@ -0,0 +1,42 @@
+import Section from "./Section";
+import Button from "./Button";
+import { curve, heroBackground, robot } from "../assets";
+
+const Hero = () => {
+  return (
+    <Section
+      className="pt-[12rem] -mt-[5.35]"
+      crosses
+      crossesOffset="lg:translate-y-[5.25rem]"
+      customPaddings
+      id="hero"
+    >
+      <div className="container relative">
+        <div className="relative z-1 max-w-[62rem] mx-auto text-center mb-[4rem] md:mb-20 lg:mb:[6rem]">
+          <h1 className="h1 mb-6">
+            Explore the Possibilities of AI Chatting with
+            <span className="inline-block relative">
+              Brainwave
+              <img
+                src={curve}
+                className="absolute top-full left-0 w-full xl:-mt-2"
+                width={624}
+                height={28}
+                alt="Curve"
+              />
+            </span>
+          </h1>
+          <p className="body-1 max-w-3xl mx-auto mb-6 text-n-2 lg:mb-8">
+            Unleash the power of AI within Brainwave. Upgrade your productivity
+            with Brainwave, the open AI chat app.
+          </p>
+          <Button href={"/pricing"} white>
+            Get started
+          </Button>
+        </div>
+      </div>
+    </Section>
+  );
+};
+
+export default Hero;

c25ace026649d150a32c58347cdc4cc142546524
Aaron Paul Greyling <aaron.p.greyling@gmail.com>
2024-05-20 15:55:54 +0200

    Completed Pricing Section

--- src/components/PricingList.jsx
+++ src/components/PricingList.jsx
@@ -8,7 +8,7 @@ const PricingList = () => {
       {pricing.map((item) => (
         <div
           key={item.id}
-          className="w-[19rem] max-lg:w-full h-full px-6 bg-n-8 border border-n-6 rounded-[2rem] lg:w-auto even:py-14 odd:py-8"
+          className="w-[19rem] max-lg:w-full h-full px-6 bg-n-8 border border-n-6 rounded-[2rem] lg:w-auto even:py-14 odd:py-8 odd:my-4 [&>h4]:first:text-color-2 [&>h4]:even:text-color-1 [&>h4]:last:text-color-3"
         >
           <h4 className="h4 mb-4">{item.title}</h4>
           <p className="body-2 min-h-[4rem] mb-3 text-n-1/50">
@@ -18,8 +18,10 @@ const PricingList = () => {
           <div className="flex items-center h-[5.5rem] mb-6">
             {item.price && (
               <>
-                <div>$</div>
-                <div>{item.price}</div>
+                <div className="h3">$</div>
+                <div className="text-[5.5rem] leading-none font-bold">
+                  {item.price}
+                </div>
               </>
             )}
           </div>
@@ -33,9 +35,12 @@ const PricingList = () => {
 
           <ul>
             {item.features.map((feature, index) => (
-              <li key={index}>
+              <li
+                key={index}
+                className="flex items-start py-5 border-t border-n-6"
+              >
                 <img src={check} width={24} height={24} alt="Check" />
-                <p>{feature}</p>
+                <p className="body-2 ml-4">{feature}</p>
               </li>
             ))}
           </ul>

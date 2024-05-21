4c4463471969a0aed2f09ecb1844e7a69b09d470
Aaron Paul Greyling <aaron.p.greyling@gmail.com>
2024-05-21 22:18:33 +0200

    Created Footer Component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import Section from "./Section";
+
+const Footer = () => {
+  return (
+    <Section crosses className="!px-0 !py-10">
+      <div className=""></div>
+    </Section>
+  );
+};
+
+export default Footer;
